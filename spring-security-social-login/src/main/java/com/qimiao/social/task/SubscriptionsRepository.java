982b70ec398e4f33b004615a34a86ad7f6950417
zhouhaojie <zhouhaojie@trendit.cn>
2024-08-14 00:12:34 +0800

    订阅任务

--- spring-security-social-login/src/main/java/com/qimiao/social/task/SubscriptionsRepository.java
+++ spring-security-social-login/src/main/java/com/qimiao/social/task/SubscriptionsRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface SubscriptionsRepository extends JpaRepository<SubscriptionsEntity, Long> {
+    // 删除日历时. 把订阅关系删掉
+    void deleteAllByClientRegistrationIdAndCalvId(String clientRegistrationId, String calvId);
 
     SubscriptionsEntity findBySubscriptionIdAndResourceId(String subscriptionId, String resourceId);
 
+    @Deprecated
     SubscriptionsEntity findByClientRegistrationIdAndCalvIdAndPrincipalName(String registrationId, String calvId, String principalName);
 }
