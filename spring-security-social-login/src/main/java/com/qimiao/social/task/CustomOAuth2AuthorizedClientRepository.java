07cb7afb516c6647e4886df0395bd39ae449bed3
zhouhaojie <okzhouhaojie@gmail.com>
2024-11-14 20:05:26 +0800

    fix error

--- spring-security-social-login/src/main/java/com/qimiao/social/task/CustomOAuth2AuthorizedClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.qimiao.social.task;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomOAuth2AuthorizedClientRepository extends JpaRepository<OAuth2AuthorizedClientEntity, ClientPrincipal> {
-}
